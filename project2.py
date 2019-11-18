d7fb5fbb3a25039254d1124471632b10149b2137
Will deBruynKops <willdbk@gmail.com>
2019-11-17 21:55:58 -0500

    grouped points into lists

--- project2.py
+++ project2.py
@@ -4,7 +4,7 @@ import scipy.linalg as sla
 import matplotlib.pyplot as plt
 
 class FluidFlow:
-    def __init__(self, V_0, nu, a, b, c, d, L, w):
+    def __init__(self, V_0, nu, a, b, c, d, L):
         self.V_0 = V_0
         self.nu = nu
         self.a = a #large side length
@@ -13,84 +13,118 @@ class FluidFlow:
         self.d = d #height of obstruction
         self.L = L # L is number of grid points along a
         self.h = a/L
-        self.w = w # w is the overrelaxation factor
         self.e_points = math.ceil(L*b/a)+1 #number points on E boundary
         self.c_points = math.ceil(L*c/a)+1 #'' ''
         self.a_points = int(L*(a-b-c)/a)+1#'' ''
         self.f_points=L+1#'' ''
         self.d_points=int(L*d/a)
         self.h_points=math.ceil(L*(a-d)/a)+1 #number of points above obstruction
-        print("e_points: " + str(self.e_points))
-        print("c_points: " + str(self.c_points))
-        print("a_points: " + str(self.a_points))
-        print("f_points: " + str(self.f_points))
-        print("d_points: " + str(self.d_points))
-        print("h_points: " + str(self.h_points))
+        # print("e_points: " + str(self.e_points))
+        # print("c_points: " + str(self.c_points))
+        # print("a_points: " + str(self.a_points))
+        # print("f_points: " + str(self.f_points))
+        # print("d_points: " + str(self.d_points))
+        # print("h_points: " + str(self.h_points))
+        self.N = self.get_superindex(L, L) + 1
+        self.psi_values = np.zeros((self.N)) #stream function matrix
+        self.omega_values = np.zeros((self.N)) #vorticity matrix
+        self.sortboundaries()
 
-    def create_matrix(self):
-        L=self.L
-        #grid before obstruction
-        N = self.get_superindex(self.L, self.L) + 1
-        phi_matrix = np.zeros((N, N)) #stream function matrix
-        omega_matrix = np.zeros((N, N)) #vorticity matrix
-        phi_b = np.zeros((N))
-        omega_b = np.zeros((N))
-        # fix all boundary conditions in which phi or omega are zero
-        for I in range(N):
-            i, j = self.get_coords(I)
-            if self.onboundary(I):
-                phi_matrix[I][I] = 1.0
-                omega_matrix[I][I] = 1.0
-                # b[I] = boundary(I)
-
-            else:
-                A[I][I] = -4.0
-                A[I][self.get_superindex(i, j+1)] = 1.0
-                A[I][self.get_superindex(i, j-1)] = 1.0
-                A[I][self.get_superindex(i+1, j)] = 1.0
-                A[I][self.get_superindex(i-1, j)] = 1.0
-                b[I] = self.omega_matrix[I][I]
-        self.A = A
+     # n is the number of relaxations performed, w is the overrelaxation factor
+    def SOR(self, n, w=1.5):
+        self.initialize_free_flow()
+        self.apply_boundary_cond(["A", "B", "C", "D", "E", "F", "G", "H"])
+        for i in range(n):
+            self.update_psi_interior()
+            self.update_omega_interior()
+            self.apply_boundary_cond(["B", "C", "D", "H"])
 
     def initialize_free_flow(self):
-        N = self.get_superindex(self.L, self.L) + 1
-        for I in range(N):
+        for I in range(self.N):
             i, j = self.get_coords(I)
-            self.phi_matrix[I][I] = 1
-            self.omega_matrix[I][I] = 1
-            self.phi_b[I] = V_0 * a*j/L
-            self.omega_b[I] = 0
+            self.psi_values[I] = self.V_0 * self.a*j/self.L
+            self.omega_values[I] = 0
 
-    def fix_boundaries(self):
-        V_0=self.V_0
-        L = self.L
-        # fix A and E boundaries
-        for i in range(L+1):
-            if(i < self.e_points or i > self.e_points + self.c_points-2):
-                I = self.get_superindex(i, 0)
-                self.phi_b[I] = 0
-            else: #C bound
-                prev_I = self.get_superindex()
-                self.omega_b[I] = -2/(self.h**2))
+    def apply_boundary_cond(self, boundaries):
+        for boundary in boundaries:
+            if boundary == "A":
+                for I in self.Abound_points:
+                    self.psi_values[I] = 0
+                    self.omega_values[I] = 0
+            if boundary == "E":
+                for I in self.Ebound_points:
+                    self.psi_values[I] = 0
+                    self.omega_values[I] = 0
+            if boundary == "C":
+                for I in self.Cbound_points:
+                    i, j = self.get_coords(I)
+                    psi = self.psi_values[self.get_superindex(i, j + 1)]
+                    self.omega_values[I] = -2/(self.h**2)*psi
+                    self.psi_values[I] = 0
+            if boundary == "B":
+                for I in self.Bbound_points:
+                    i, j = self.get_coords(I)
+                    psi = self.psi_values[self.get_superindex(i - 1, j)]
+                    self.psi_values[I] = 0
+                    self.omega_values[I] = -2/(self.h**2)*psi
+            if boundary == "D":
+                for I in self.Dbound_points:
+                    i, j = self.get_coords(I)
+                    psi = self.psi_values[self.get_superindex(i + 1, j)]
+                    self.psi_values[I] = 0
+                    self.omega_values[I] = -2/(self.h**2)*psi
+            if boundary == "G": #set to free flow conditions
+                for I in self.Gbound_points:
+                    i, j = self.get_coords(I)
+                    self.psi_values[I] = self.V_0 * self.a*j/self.L
+                    self.omega_values[I] = 0
+            if boundary == "F": #set to free flow conditions
+                for I in self.Fbound_points:
+                    i, j = self.get_coords(I)
+                    self.psi_values[I] = self.V_0 * self.a*j/self.L
+                    self.omega_values[I] = 0
+            if boundary == "H":
+                for I in self.Hbound_points:
+                    i, j = self.get_coords(I)
+                    I_left = self.get_superindex(i-1, j)
+                    self.psi_values[I] = self.psi_values[I_left]
+                    self.omega_values[I] = self.omega_values[I_left]
 
-        # fix B and D boundaries
-        for
-        # fix G boundary
-        for i in range(L+1):
-            I = self.get_superindex(i, L)
-            self.phi_matrix[I][I] = 1
-            self.omega_matrix[I][I] = 1
-            self.phi_b[I] = V_0 * a
-            self.omega_b[I] = 0
-        #
+    def update_psi_interior(self, w):
+        for I in self.Interior_points:
+            i, j = self.get_coords(I)
+            self.psi_values[I] = (1-w)*self.psi_values[I] + \
+                (w/4)*(self.psi_values[self.get_superindex(i+1,j)] + \
+                self.psi_values[self.get_superindex(i-1,j)] + \
+                self.psi_values[self.get_superindex(i,j+1)] + \
+                self.psi_values[self.get_superindex(i,j-1)] - \
+                self.omega_values[I])
+
+    def update_omega_interior(self, w):
+        for I in self.Interior_points:
+            i, j = self.get_coords(I)
+            d_psi_d_y = self.psi_values[self.get_superindex(i,j+1)] - \
+                self.psi_values[self.get_superindex(i,j-1)]
+            d_omega_d_y = self.omega_values[self.get_superindex(i,j+1)] - \
+                self.omega_values[self.get_superindex(i,j-1)]
+            d_psi_d_x = self.psi_values[self.get_superindex(i+1,j)] - \
+                self.psi_values[self.get_superindex(i-1,j)]
+            d_omega_d_x = self.omega_values[self.get_superindex(i+1,j)] - \
+                self.omega_values[self.get_superindex(i-1,j)]
+
+            self.omega_values[I] = (1-w)*self.omega_values[I] + \
+                (w/4)*(self.omega_values[self.get_superindex(i+1,j)] + \
+                self.omega_values[self.get_superindex(i-1,j)] + \
+                self.omega_values[self.get_superindex(i,j+1)] + \
+                self.omega_values[self.get_superindex(i,j-1)] - \
+                (1/(2*nu))*(d_psi_d_y*d_omega_d_x - d_psi_d_x*d_omega_d_y))
 
 
-
-    def onboundary(self,I,fulloutput=False): #checks if point on onboundary
-        if fulloutput==False: #just checks if it's on a boundary
-            onbound=False
-            L=self.L
-            i,j=self.get_coords(I)
+    def onboundary(self,I,specific=False): #checks if point on onboundary
+        onbound=False
+        L=self.L
+        i,j=self.get_coords(I)
+        if specific==False:
             if i==0 or i==L or j==0 or j==L: #F boundary
                 onbound=True
             elif i==self.e_points-1 or i==self.e_points+self.c_points-2:
@@ -100,78 +134,114 @@ class FluidFlow:
                 onbound=True
             return onbound
         else:
-            if i==0:
+            if i==0: #F boundary
+                whichbound='F'
+            elif i==L: #H boundary
+                whichbound='H'
+            elif j==0 and i<=self.e_points-1: #E boundary
+                whichbound='E'
+            elif j==0 and i>self.e_points+self.c_points-2: #A boundary
+                whichbound='A'
+            elif j==L: #G boundary
+                whichbound='G'
+            elif j==self.e_points-1 and i<=self.d_points: #D boundary
+                whichbound='D'
+            elif j==self.e_points+self.c_points-2 and i<=self.d_points:
+                whichbound='B'
+            else:
+                whichbound='C'
+            return whichbound
 
+    def sortboundaries(self): #Imax is total number of verticies
+        #sorts the verticies into different lists
+        self.Interior_points=[]
+        self.Abound_points=[]
+        self.Bbound_points=[]
+        self.Cbound_points=[]
+        self.Dbound_points=[]
+        self.Ebound_points=[]
+        self.Fbound_points=[]
+        self.Gbound_points=[]
+        self.Hbound_points=[]
+        for I in range(self.N):
+            if self.onboundary(I)==False:
+                self.Interior_points.append(I)
+            else:
+                if self.onboundary(I,True)=='A':
+                    self.Abound_points.append(I)
+                elif self.onboundary(I,True)=='B':
+                    self.Bbound_points.append(I)
+                elif self.onboundary(I,True)=='C':
+                    self.Cbound_points.append(I)
+                elif self.onboundary(I,True)=='D':
+                    self.Dbound_points.append(I)
+                elif self.onboundary(I,True)=='E':
+                    self.Ebound_points.append(I)
+                elif self.onboundary(I,True)=='F':
+                    self.Fbound_points.append(I)
+                elif self.onboundary(I,True)=='G':
+                    self.Gbound_points.append(I)
+                elif self.onboundary(I,True)=='H':
+                    self.Hbound_points.append(I)
 
     def indextest(self):
         N = self.get_superindex(self.L, self.L) + 1
-        for I in range(N):
+        for I in range(self.N):
             i, j = self.get_coords(I)
             print(str(I) + " (i, j) = "  + str(i) + ", " + str(j) + "  I=" + str(self.get_superindex(i, j)))
 
 
     def get_superindex(self, i, j):
-        e_points = self.e_points
-        c_points = self.c_points
-        a_points = self.a_points
-        f_points=self.f_points
-        d_points=self.d_points
-        h_points=self.h_points
-        if i < e_points: #shifted to 0, incudes D boundary
-            return i*f_points + j
-        elif i < e_points + c_points - 2: #above obstruction
-            if j >= d_points:
-                Nbefore=f_points*e_points
-                new_i = i - e_points
-                new_j = j - d_points
-                return Nbefore + new_i*(h_points) + new_j
+        if i < self.e_points: #shifted to 0, incudes D boundary
+            return i*self.f_points + j
+        elif i < self.e_points + self.c_points - 2: #above obstruction
+            if j >= self.d_points:
+                Nbefore=self.f_points*self.e_points
+                new_i = i - self.e_points
+                new_j = j - self.d_points
+                return Nbefore + new_i*(self.h_points) + new_j
             else:
                 raise Exception("Inaccessible index")
         else:
-            Nbefore = f_points*e_points + h_points*(c_points-2)
-            new_i = i - e_points - (c_points - 2)
-            return Nbefore + new_i*(f_points) + j
+            Nbefore = self.f_points*self.e_points + self.h_points*(self.c_points-2)
+            new_i = i - self.e_points - (self.c_points - 2)
+            return Nbefore + new_i*(self.f_points) + j
 
     def get_coords(self, I): #gets coordinates from superindex I
-        e_points = self.e_points
-        c_points = self.c_points
-        a_points = self.a_points
-        f_points=self.f_points
-        d_points=self.d_points
-        h_points=self.h_points
-        if I < f_points*e_points:
-            i = int(I/f_points)
-            j = I%(f_points)
+        if I < self.f_points*self.e_points:
+            i = int(I/self.f_points)
+            j = I%(self.f_points)
             return i,j
-        elif I < f_points*e_points + h_points*(c_points-2):
-            new_I = I - f_points*e_points
-            i = int(new_I/h_points) + e_points
-            j = new_I%(h_points) + d_points
+        elif I < self.f_points*self.e_points + self.h_points*(self.c_points-2):
+            new_I = I - self.f_points*self.e_points
+            i = int(new_I/self.h_points) + self.e_points
+            j = new_I%(self.h_points) + self.d_points
             return i,j
         else:
-            new_I = I - f_points*e_points - h_points*(c_points-2)
-            i = int(new_I/f_points) + e_points + (c_points - 2)
-            j = new_I%(f_points)
+            new_I = I - self.f_points*self.e_points - self.h_points*(self.c_points-2)
+            i = int(new_I/self.f_points) + self.e_points + (self.c_points - 2)
+            j = new_I%(self.f_points)
             return i,j
 
-    # n is the number of iterations to perform SOR
-    def SOR(n):
-        return
+    def print_psi(self):
+        matrix = np.zeros((self.L+1, self.L+1))
+        for I in range(self.N):
+            i, j = self.get_coords(I)
+            matrix[self.L - j][i] = self.psi_values[I]
+        print(matrix)
 
 def main():
     V_0 = 1
     nu = 0.1
-    size_of_plate = 1.0
-    front_of_plate = 0.33
-    back_of_plate = 0.66
-    top_of_plate = 0.33
+    region_dim = 1.0
+    front_of_plate = 0.0
+    back_of_plate = 0.0
+    top_of_plate = 0.0
     L = 3
-    w = 1.5
-    print()
-    ff = FluidFlow(V_0, nu, size_of_plate, front_of_plate,
-        back_of_plate - front_of_plate, top_of_plate, L, w)
-    ff.create_matrix()
-    print(ff.A)
+    ff = FluidFlow(V_0, nu, region_dim, front_of_plate,
+        back_of_plate - front_of_plate, top_of_plate, L)
+    ff.SOR(0)
+    print(ff.print_psi())
 
 if __name__ == "__main__":
     main()
