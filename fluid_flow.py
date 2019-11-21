3db40cad3cd0aac5155b5bd289401254d521e154
Will deBruynKops <willdbk@gmail.com>
2019-11-21 12:40:24 -0500

    added comments and cleaned up
    pretty much ready to turn in i'd say

--- fluid_flow.py
+++ fluid_flow.py
@@ -6,40 +6,46 @@ from mpl_toolkits.mplot3d import Axes3D
 from scipy.integrate import quad
 import sys
 
+# The FluidFlow class models the fluid flow around an obstruction by creating
+# a grid that computes values of psi and vorticity at each discrete gridpoint.
+# These values are computed using the Successive Overrelation Method (SOR).
 class FluidFlow:
-    # b is length before boundary starts
-    # c is width of obstruction
-    # d is height of obstruction
+    # a is the length of the square plate, b is length before boundary starts,
+    # c is width of obstruction, and d is height of obstruction
     def __init__(self, V_0, nu, a, b, c, d, L):
         self.V_0 = V_0
         self.nu = nu
-        self.a = a #large side length
-
+        self.a = a
         self.L = L # L is number of grid points along each side
         self.h = a/L # h is the grid spacing
 
         self.before_plate = math.ceil(L*b/a)
         self.on_plate = math.ceil(L*c/a)
-        self.after_plate = int(L*(a-b-c)/a)
-        assert(self.before_plate + self.on_plate + self.after_plate == L + 1)
-
+        self.after_plate = L + 1 - (self.before_plate + self.on_plate)
         self.plate_height = int(L*d/a)
 
-        self.psi_matrix = np.zeros((L+1,L+1)) #stream function matrix
-        self.omega_matrix = np.zeros((L+1,L+1)) #vorticity matrix
-        self.residual_matrix = np.zeros((L+1,L+1)) #residual matrix
         self.sortboundaries()
+
+    def initialize_grid(self):
+        self.psi_matrix = np.zeros((self.L+1,self.L+1)) #stream function matrix
+        self.omega_matrix = np.zeros((self.L+1,self.L+1)) #vorticity matrix
+        self.residual_matrix = np.zeros((self.L+1,self.L+1)) #residual matrix
+        self.initialize_free_flow()
+        self.apply_boundary_cond(["A", "E", "F", "G", "H"])
+
 
      # n is the number of relaxations performed, w is the overrelaxation factor
     def SOR(self, n, w = 1.5):
-        self.initialize_free_flow()
-        self.apply_boundary_cond(["A", "E", "F", "G", "H"])
+        self.initialize_grid()
         for i in range(n):
             self.update_psi_interior(w)
             self.apply_boundary_cond(["B", "C", "D"])
             self.update_omega_interior(w)
             self.apply_boundary_cond(["H"])
+        self.update_residual()
 
+    # This function updates the interior values of psi according to the
+    # overrelaxation factor w.
     def update_psi_interior(self, w):
         for point in self.Interior_points:
             i, j = point[0], point[1]
@@ -50,6 +56,8 @@ class FluidFlow:
                 self.psi_matrix[i][j-1] + \
                 self.h*self.h*self.omega_matrix[i][j])
 
+    # This function updates the interior values of omega according to the
+    # overrelaxation factor w.
     def update_omega_interior(self, w):
         for point in self.Interior_points:
             i, j = point[0], point[1]
@@ -61,16 +69,19 @@ class FluidFlow:
             self.omega_matrix[i][j] = (1-w)*self.omega_matrix[i][j] + \
                 (w/4)*(self.omega_matrix[i+1][j] + self.omega_matrix[i-1][j] + \
                 self.omega_matrix[i][j+1] + self.omega_matrix[i][j-1] + \
-                self.h*self.h*(1/(self.nu))*(d_psi_d_y*d_omega_d_x - d_psi_d_x*d_omega_d_y))
+                (self.h*self.h/self.nu)*(d_psi_d_y*d_omega_d_x - d_psi_d_x*d_omega_d_y))
 
-    def compute_residual(self):
+    # This function computes the residual in the interior points and stores
+    # these values in self.residual_matrix.
+    def update_residual(self):
         for point in self.Interior_points:
             i, j = point[0], point[1]
-            self.residual_matrix[i][j] = -4*self.psi_matrix[i][j] + \
+            self.residual_matrix[i][j] = (-4*self.psi_matrix[i][j] + \
             self.psi_matrix[i+1][j] + self.psi_matrix[i-1][j] + \
-            self.psi_matrix[i][j+1] + self.psi_matrix[i][j-1] + \
-            self.h*self.h*self.omega_matrix[i][j]
+            self.psi_matrix[i][j+1] + self.psi_matrix[i][j-1])/(self.h*self.h) + \
+            self.omega_matrix[i][j]
 
+    # This function returns the overall norm of the residual matrix.
     def residual_norm(self):
         sum = 0
         for i in range(self.L+1):
@@ -87,52 +98,52 @@ class FluidFlow:
     def apply_boundary_cond(self, boundaries):
         for boundary in boundaries:
             if boundary == "A":
-                for coords in self.Abound_points:
-                    i, j = coords[0], coords[1]
+                for point in self.Abound_points:
+                    i, j = point[0], point[1]
                     self.psi_matrix[i][j] = 0
                     self.omega_matrix[i][j] = 0
             if boundary == "E":
-                for coords in self.Ebound_points:
-                    i, j = coords[0], coords[1]
+                for point in self.Ebound_points:
+                    i, j = point[0], point[1]
                     self.psi_matrix[i][j] = 0
                     self.omega_matrix[i][j] = 0
             if boundary == "C":
-                for coords in self.Cbound_points:
-                    i, j = coords[0], coords[1]
+                for point in self.Cbound_points:
+                    i, j = point[0], point[1]
                     psi = self.psi_matrix[i][j+1]
                     self.omega_matrix[i][j] = -2/(self.h**2)*psi
                     self.psi_matrix[i][j] = 0
             if boundary == "B":
-                for coords in self.Bbound_points:
-                    i, j = coords[0], coords[1]
+                for point in self.Bbound_points:
+                    i, j = point[0], point[1]
                     psi = self.psi_matrix[i+1][j]
                     self.omega_matrix[i][j] = -2/(self.h**2)*psi
                     self.psi_matrix[i][j] = 0
             if boundary == "D":
-                for coords in self.Dbound_points:
-                    i, j = coords[0], coords[1]
+                for point in self.Dbound_points:
+                    i, j = point[0], point[1]
                     psi = self.psi_matrix[i-1][j]
                     self.omega_matrix[i][j] = -2/(self.h**2)*psi
                     self.psi_matrix[i][j] = 0
             if boundary == "G": #set to free flow conditions
-                for coords in self.Gbound_points:
-                    i, j = coords[0], coords[1]
+                for point in self.Gbound_points:
+                    i, j = point[0], point[1]
                     self.psi_matrix[i][j] = self.V_0 * self.a*j/self.L
                     self.omega_matrix[i][j] = 0
             if boundary == "F": #set to free flow conditions
-                for coords in self.Fbound_points:
-                    i, j = coords[0], coords[1]
+                for point in self.Fbound_points:
+                    i, j = point[0], point[1]
                     self.psi_matrix[i][j] = self.V_0 * self.a*j/self.L
                     self.omega_matrix[i][j] = 0
             if boundary == "H":
-                for coords in self.Hbound_points:
-                    i, j = coords[0], coords[1]
+                for point in self.Hbound_points:
+                    i, j = point[0], point[1]
                     self.psi_matrix[i][j] = self.psi_matrix[i-1][j]
                     self.omega_matrix[i][j] = self.omega_matrix[i-1][j]
 
-
-    def sortboundaries(self): #Imax is total number of verticies
-        #sorts the verticies into different lists
+    # This function sorts each vertex as a pair of coordinates into whatever
+    # boundary it lies on or into the interior.
+    def sortboundaries(self):
         L=self.L
         self.Interior_points=[]
         self.Plate_interior_points=[]
@@ -198,27 +209,40 @@ class FluidFlow:
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
         ax.plot_surface(X, Y, self.residual_matrix, cmap="viridis")
-        ax.set_xlabel(r'$x$')
-        ax.set_ylabel(r'$y$')
+
+        ax.set_xlabel(r'$y$')
+        ax.set_ylabel(r'$x$')
         ax.set_zlabel(r'Residual')
         ax.set_title(r"Surface plot of the residual versus $x$ and $y$")
         plt.show()
 
-def residual_norm_vs_w():
-    V_0 = 1
-    nu = 0.1
-    region_dim = 1.0
-    front_of_plate = 0.25
-    back_of_plate = 0.55
-    top_of_plate = 0.5
-    L = int(sys.argv[1])
-    num_relaxations = int(sys.argv[2])
-    for i in range(3):
-        w = 0.25 + 0.25*i
-        ff = FluidFlow(V_0, nu, region_dim, front_of_plate, back_of_plate - front_of_plate, top_of_plate, L)
-        ff.SOR(num_relaxations, w)
-        ff.compute_residual()
-        print("residual norm: " + str(ff.residual_norm()))
+    def fluid_flow_contour_plot(self):
+        x_values = np.linspace(0, 1.0, self.L+1)
+        y_values = np.linspace(0, 1.0, self.L+1)
+        X, Y = np.meshgrid(x_values, y_values)
+
+        fig,ax=plt.subplots(1,1)
+        cp = ax.contourf(Y, X, self.psi_matrix)
+        fig.colorbar(cp) # Add a colorbar to a plot
+        ax.set_title('Filled Contours Plot')
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+        ax.set_title(r"Contour plot of the psi versus $x$ and $y$")
+        plt.show()
+
+    def residual_norm_vs_w(self, w_0, w_f, n, num_relaxations):
+        w_values = np.linspace(w_0, w_f, n)
+        res_norm_values = np.zeros((n))
+        for i in range(n):
+            self.SOR(num_relaxations, w_values[i])
+            res_norm_values[i] = self.residual_norm()
+        print("optimal value of w: " + str(w_values[np.argmin(res_norm_values)]))
+        plt.plot(w_values, res_norm_values)
+        plt.xlabel(r'$w$')
+        plt.ylabel(r'Residual Norm $R$')
+        plt.title(r'Plot of Residual Norm $R$ as a function of $w$')
+        plt.show()
+
 
 def main():
     V_0 = 1
@@ -226,15 +250,17 @@ def main():
     region_dim = 1.0
     front_of_plate = 0.25
     back_of_plate = 0.55
-    top_of_plate = 0.5
+    top_of_plate = 0.3
     L = int(sys.argv[1])
     ff = FluidFlow(V_0, nu, region_dim, front_of_plate,
         back_of_plate - front_of_plate, top_of_plate, L)
     num_relaxations = int(sys.argv[2])
+
     ff.SOR(num_relaxations)
-    ff.compute_residual()
-    print("residual norm: " + str(ff.residual_norm()))
+
     ff.graph_residual()
+    ff.fluid_flow_contour_plot()
+    ff.residual_norm_vs_w(1.45, 1.6, 10, num_relaxations)
 
 
 if __name__ == "__main__":
